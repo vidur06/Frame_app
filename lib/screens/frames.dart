37e64f4266499e425613815e51ff296e2c3a7591
dipakbaldaniya07 <104146813+dipakbaldaniya07@users.noreply.github.com>
2022-10-13 11:49:02 +0530

    Update frames.dart

--- lib/screens/frames.dart
+++ lib/screens/frames.dart
@@ -91,9 +91,7 @@ class _FramesState extends State<Frames> {
             child: InkWell(
               onTap: () {
                 interstitialAds();
-                // if (isLoaded) {
-                //   interstitialAd.show();
-                // }
+             
                 Navigator.of(context).pushNamed(
                   "framepage",
                   arguments: [
